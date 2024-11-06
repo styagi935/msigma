c577682d8f190499593190b87d892f67ef8fd990
Shubham Tyagi <111732194+styagi935@users.noreply.github.com>
2024-11-06 18:54:47 +0530

    Update dockerfile.sql

--- dockerfile.sql
+++ dockerfile.sql
@@ -4,7 +4,7 @@ FROM mysql:5.7
 # Set environment variables for database configuration
 ENV MYSQL_DATABASE=wordpress \
     MYSQL_USER=wordpress_user \
-    MYSQL_PASSWORD=securepassword \
-    MYSQL_ROOT_PASSWORD=rootpassword
+    MYSQL_PASSWORD=$securepassword \
+    MYSQL_ROOT_PASSWORD=$rootpassword
 
 
